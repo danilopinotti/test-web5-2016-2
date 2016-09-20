932b61d72b012a986b937ea68add3d2510cabaf0
Danilo Pinotti <danilopinotti@hotmail.com>
2016-09-20 15:24:02 -0300

    more tests

--- test/units/velocity_test.rb
+++ test/units/velocity_test.rb
@@ -7,99 +7,407 @@ class VelocityTest < Minitest::Test
     assert_equal 1, 1
   end
 
-  def test_velocity_kh_to_knot_0
-    assert_equal 442.23, Velocity::Velocity.new(819.0, 'kh').to_knot
+  def test_velocity_ms_to_knot_0
+    assert_equal 15.55, Velocity::Velocity.new(8.0, 'ms').to_knot
   end
 
-  def test_velocity_knot_to_kh_1
-    assert_equal 2_818.74, Velocity::Velocity.new(1_522.0, 'knot').to_kh
+  def test_velocity_knot_to_ms_1
+    assert_equal 44.76, Velocity::Velocity.new(87.0, 'knot').to_ms
   end
 
-  def test_velocity_mph_to_kh_2
-    assert_equal 1_002.62, Velocity::Velocity.new(623.0, 'mph').to_kh
+  def test_velocity_mph_to_ms_2
+    assert_equal 4.47, Velocity::Velocity.new(10.0, 'mph').to_ms
   end
 
   def test_velocity_kh_to_mph_3
-    assert_equal 205.05, Velocity::Velocity.new(330.0, 'kh').to_mph
+    assert_equal 60.89, Velocity::Velocity.new(98.0, 'kh').to_mph
   end
 
   def test_velocity_mph_to_mph_4
-    assert_equal 665.0, Velocity::Velocity.new(665.0, 'mph').to_mph
+    assert_equal 98.0, Velocity::Velocity.new(98.0, 'mph').to_mph
   end
 
-  def test_velocity_ms_to_kh_5
-    assert_equal 5_979.6, Velocity::Velocity.new(1_661.0, 'ms').to_kh
+  def test_velocity_knot_to_ms_5
+    assert_equal 12.35, Velocity::Velocity.new(24.0, 'knot').to_ms
   end
 
-  def test_velocity_mph_to_knot_6
-    assert_equal 1_564.16, Velocity::Velocity.new(1_800.0, 'mph').to_knot
+  def test_velocity_mph_to_ms_6
+    assert_equal 26.82, Velocity::Velocity.new(60.0, 'mph').to_ms
   end
 
-  def test_velocity_ms_to_mph_7
-    assert_equal 3_169.74, Velocity::Velocity.new(1_417.0, 'ms').to_mph
+  def test_velocity_kh_to_mph_7
+    assert_equal 5.59, Velocity::Velocity.new(9.0, 'kh').to_mph
   end
 
-  def test_velocity_mph_to_mph_8
-    assert_equal 1_269.0, Velocity::Velocity.new(1_269.0, 'mph').to_mph
+  def test_velocity_knot_to_ms_8
+    assert_equal 1.03, Velocity::Velocity.new(2.0, 'knot').to_ms
   end
 
-  def test_velocity_mph_to_ms_9
-    assert_equal 878.88, Velocity::Velocity.new(1_966.0, 'mph').to_ms
+  def test_velocity_knot_to_kh_9
+    assert_equal 7.41, Velocity::Velocity.new(4.0, 'knot').to_kh
   end
 
-  def test_velocity_mph_to_kh_10
-    assert_equal 696.85, Velocity::Velocity.new(433.0, 'mph').to_kh
+  def test_velocity_kh_to_knot_10
+    assert_equal 7.02, Velocity::Velocity.new(13.0, 'kh').to_knot
   end
 
-  def test_velocity_knot_to_ms_11
-    assert_equal 401.27, Velocity::Velocity.new(780.0, 'knot').to_ms
+  def test_velocity_knot_to_knot_11
+    assert_equal 4.0, Velocity::Velocity.new(4.0, 'knot').to_knot
   end
 
   def test_velocity_knot_to_kh_12
-    assert_equal 1_066.75, Velocity::Velocity.new(576.0, 'knot').to_kh
+    assert_equal 124.08, Velocity::Velocity.new(67.0, 'knot').to_kh
   end
 
   def test_velocity_kh_to_kh_13
-    assert_equal 1_215.0, Velocity::Velocity.new(1_215.0, 'kh').to_kh
+    assert_equal 47.0, Velocity::Velocity.new(47.0, 'kh').to_kh
   end
 
-  def test_velocity_ms_to_kh_14
-    assert_equal 2_106.0, Velocity::Velocity.new(585.0, 'ms').to_kh
+  def test_velocity_knot_to_knot_14
+    assert_equal 14.0, Velocity::Velocity.new(14.0, 'knot').to_knot
   end
 
   def test_velocity_mph_to_kh_15
-    assert_equal 2_499.31, Velocity::Velocity.new(1_553.0, 'mph').to_kh
+    assert_equal 128.75, Velocity::Velocity.new(80.0, 'mph').to_kh
   end
 
-  def test_velocity_knot_to_kh_16
-    assert_equal 2_303.88, Velocity::Velocity.new(1_244.0, 'knot').to_kh
+  def test_velocity_knot_to_mph_16
+    assert_equal 108.17, Velocity::Velocity.new(94.0, 'knot').to_mph
   end
 
-  def test_velocity_ms_to_mph_17
-    assert_equal 1_026.75, Velocity::Velocity.new(459.0, 'ms').to_mph
+  def test_velocity_kh_to_knot_17
+    assert_equal 34.02, Velocity::Velocity.new(63.0, 'kh').to_knot
   end
 
-  def test_velocity_knot_to_knot_18
-    assert_equal 1_563.0, Velocity::Velocity.new(1_563.0, 'knot').to_knot
+  def test_velocity_mph_to_kh_18
+    assert_equal 90.12, Velocity::Velocity.new(56.0, 'mph').to_kh
   end
 
-  def test_velocity_knot_to_ms_19
-    assert_equal 757.78, Velocity::Velocity.new(1_473.0, 'knot').to_ms
+  def test_velocity_ms_to_knot_19
+    assert_equal 58.32, Velocity::Velocity.new(30.0, 'ms').to_knot
   end
 
-  def test_velocity_mph_to_mph_20
-    assert_equal 445.0, Velocity::Velocity.new(445.0, 'mph').to_mph
+  def test_velocity_kh_to_mph_20
+    assert_equal 24.23, Velocity::Velocity.new(39.0, 'kh').to_mph
   end
 
-  def test_velocity_mph_to_mph_21
-    assert_equal 1_237.0, Velocity::Velocity.new(1_237.0, 'mph').to_mph
+  def test_velocity_mph_to_ms_21
+    assert_equal 19.67, Velocity::Velocity.new(44.0, 'mph').to_ms
   end
 
-  def test_velocity_kh_to_mph_22
-    assert_equal 1_128.41, Velocity::Velocity.new(1_816.0, 'kh').to_mph
+  def test_velocity_mph_to_kh_22
+    assert_equal 90.12, Velocity::Velocity.new(56.0, 'mph').to_kh
   end
 
-  def test_velocity_ms_to_mph_23
-    assert_equal 2_954.99, Velocity::Velocity.new(1_321.0, 'ms').to_mph
+  def test_velocity_kh_to_ms_23
+    assert_equal 20.56, Velocity::Velocity.new(74.0, 'kh').to_ms
+  end
+
+  def test_velocity_mph_to_mph_24
+    assert_equal 13.0, Velocity::Velocity.new(13.0, 'mph').to_mph
+  end
+
+  def test_velocity_kh_to_ms_25
+    assert_equal 4.72, Velocity::Velocity.new(17.0, 'kh').to_ms
+  end
+
+  def test_velocity_ms_to_knot_26
+    assert_equal 97.19, Velocity::Velocity.new(50.0, 'ms').to_knot
+  end
+
+  def test_velocity_knot_to_kh_27
+    assert_equal 168.53, Velocity::Velocity.new(91.0, 'knot').to_kh
+  end
+
+  def test_velocity_ms_to_kh_28
+    assert_equal 280.8, Velocity::Velocity.new(78.0, 'ms').to_kh
+  end
+
+  def test_velocity_ms_to_mph_29
+    assert_equal 127.51, Velocity::Velocity.new(57.0, 'ms').to_mph
+  end
+
+  def test_velocity_mph_to_knot_30
+    assert_equal 2.61, Velocity::Velocity.new(3.0, 'mph').to_knot
+  end
+
+  def test_velocity_knot_to_mph_31
+    assert_equal 96.67, Velocity::Velocity.new(84.0, 'knot').to_mph
+  end
+
+  def test_velocity_mph_to_knot_32
+    assert_equal 75.6, Velocity::Velocity.new(87.0, 'mph').to_knot
+  end
+
+  def test_velocity_knot_to_kh_33
+    assert_equal 111.12, Velocity::Velocity.new(60.0, 'knot').to_kh
+  end
+
+  def test_velocity_ms_to_knot_34
+    assert_equal 93.3, Velocity::Velocity.new(48.0, 'ms').to_knot
+  end
+
+  def test_velocity_kh_to_knot_35
+    assert_equal 19.44, Velocity::Velocity.new(36.0, 'kh').to_knot
+  end
+
+  def test_velocity_mph_to_mph_36
+    assert_equal 38.0, Velocity::Velocity.new(38.0, 'mph').to_mph
+  end
+
+  def test_velocity_mph_to_ms_37
+    assert_equal 43.81, Velocity::Velocity.new(98.0, 'mph').to_ms
+  end
+
+  def test_velocity_mph_to_kh_38
+    assert_equal 152.89, Velocity::Velocity.new(95.0, 'mph').to_kh
+  end
+
+  def test_velocity_kh_to_mph_39
+    assert_equal 47.22, Velocity::Velocity.new(76.0, 'kh').to_mph
+  end
+
+  def test_velocity_mph_to_ms_40
+    assert_equal 18.78, Velocity::Velocity.new(42.0, 'mph').to_ms
+  end
+
+  def test_velocity_mph_to_knot_41
+    assert_equal 47.79, Velocity::Velocity.new(55.0, 'mph').to_knot
+  end
+
+  def test_velocity_ms_to_mph_42
+    assert_equal 118.56, Velocity::Velocity.new(53.0, 'ms').to_mph
+  end
+
+  def test_velocity_ms_to_knot_43
+    assert_equal 97.19, Velocity::Velocity.new(50.0, 'ms').to_knot
+  end
+
+  def test_velocity_kh_to_kh_44
+    assert_equal 23.0, Velocity::Velocity.new(23.0, 'kh').to_kh
+  end
+
+  def test_velocity_ms_to_kh_45
+    assert_equal 172.8, Velocity::Velocity.new(48.0, 'ms').to_kh
+  end
+
+  def test_velocity_mph_to_mph_46
+    assert_equal 25.0, Velocity::Velocity.new(25.0, 'mph').to_mph
+  end
+
+  def test_velocity_mph_to_mph_47
+    assert_equal 37.0, Velocity::Velocity.new(37.0, 'mph').to_mph
+  end
+
+  def test_velocity_knot_to_knot_48
+    assert_equal 58.0, Velocity::Velocity.new(58.0, 'knot').to_knot
+  end
+
+  def test_velocity_knot_to_mph_49
+    assert_equal 108.17, Velocity::Velocity.new(94.0, 'knot').to_mph
+  end
+
+  def test_velocity_knot_to_ms_50
+    assert_equal 33.95, Velocity::Velocity.new(66.0, 'knot').to_ms
+  end
+
+  def test_velocity_kh_to_kh_51
+    assert_equal 55.0, Velocity::Velocity.new(55.0, 'kh').to_kh
+  end
+
+  def test_velocity_kh_to_knot_52
+    assert_equal 48.06, Velocity::Velocity.new(89.0, 'kh').to_knot
+  end
+
+  def test_velocity_ms_to_mph_53
+    assert_equal 35.79, Velocity::Velocity.new(16.0, 'ms').to_mph
+  end
+
+  def test_velocity_kh_to_ms_54
+    assert_equal 22.78, Velocity::Velocity.new(82.0, 'kh').to_ms
+  end
+
+  def test_velocity_ms_to_knot_55
+    assert_equal 130.24, Velocity::Velocity.new(67.0, 'ms').to_knot
+  end
+
+  def test_velocity_mph_to_ms_56
+    assert_equal 8.94, Velocity::Velocity.new(20.0, 'mph').to_ms
+  end
+
+  def test_velocity_ms_to_mph_57
+    assert_equal 199.09, Velocity::Velocity.new(89.0, 'ms').to_mph
+  end
+
+  def test_velocity_kh_to_mph_58
+    assert_equal 7.46, Velocity::Velocity.new(12.0, 'kh').to_mph
+  end
+
+  def test_velocity_knot_to_ms_59
+    assert_equal 47.84, Velocity::Velocity.new(93.0, 'knot').to_ms
+  end
+
+  def test_velocity_kh_to_kh_60
+    assert_equal 44.0, Velocity::Velocity.new(44.0, 'kh').to_kh
+  end
+
+  def test_velocity_kh_to_ms_61
+    assert_equal 0.0, Velocity::Velocity.new(0.0, 'kh').to_ms
+  end
+
+  def test_velocity_ms_to_mph_62
+    assert_equal 120.79, Velocity::Velocity.new(54.0, 'ms').to_mph
+  end
+
+  def test_velocity_kh_to_knot_63
+    assert_equal 8.64, Velocity::Velocity.new(16.0, 'kh').to_knot
+  end
+
+  def test_velocity_mph_to_knot_64
+    assert_equal 78.21, Velocity::Velocity.new(90.0, 'mph').to_knot
+  end
+
+  def test_velocity_mph_to_mph_65
+    assert_equal 94.0, Velocity::Velocity.new(94.0, 'mph').to_mph
+  end
+
+  def test_velocity_ms_to_kh_66
+    assert_equal 277.2, Velocity::Velocity.new(77.0, 'ms').to_kh
+  end
+
+  def test_velocity_knot_to_kh_67
+    assert_equal 25.93, Velocity::Velocity.new(14.0, 'knot').to_kh
+  end
+
+  def test_velocity_ms_to_ms_68
+    assert_equal 20.0, Velocity::Velocity.new(20.0, 'ms').to_ms
+  end
+
+  def test_velocity_mph_to_ms_69
+    assert_equal 41.57, Velocity::Velocity.new(93.0, 'mph').to_ms
+  end
+
+  def test_velocity_mph_to_knot_70
+    assert_equal 71.26, Velocity::Velocity.new(82.0, 'mph').to_knot
+  end
+
+  def test_velocity_mph_to_knot_71
+    assert_equal 81.68, Velocity::Velocity.new(94.0, 'mph').to_knot
+  end
+
+  def test_velocity_knot_to_kh_72
+    assert_equal 3.7, Velocity::Velocity.new(2.0, 'knot').to_kh
+  end
+
+  def test_velocity_mph_to_kh_73
+    assert_equal 144.84, Velocity::Velocity.new(90.0, 'mph').to_kh
+  end
+
+  def test_velocity_mph_to_kh_74
+    assert_equal 154.5, Velocity::Velocity.new(96.0, 'mph').to_kh
+  end
+
+  def test_velocity_knot_to_ms_75
+    assert_equal 15.43, Velocity::Velocity.new(30.0, 'knot').to_ms
+  end
+
+  def test_velocity_mph_to_mph_76
+    assert_equal 37.0, Velocity::Velocity.new(37.0, 'mph').to_mph
+  end
+
+  def test_velocity_mph_to_kh_77
+    assert_equal 27.36, Velocity::Velocity.new(17.0, 'mph').to_kh
+  end
+
+  def test_velocity_kh_to_kh_78
+    assert_equal 81.0, Velocity::Velocity.new(81.0, 'kh').to_kh
+  end
+
+  def test_velocity_knot_to_ms_79
+    assert_equal 33.95, Velocity::Velocity.new(66.0, 'knot').to_ms
+  end
+
+  def test_velocity_kh_to_ms_80
+    assert_equal 3.06, Velocity::Velocity.new(11.0, 'kh').to_ms
+  end
+
+  def test_velocity_ms_to_mph_81
+    assert_equal 85.0, Velocity::Velocity.new(38.0, 'ms').to_mph
+  end
+
+  def test_velocity_kh_to_knot_82
+    assert_equal 29.7, Velocity::Velocity.new(55.0, 'kh').to_knot
+  end
+
+  def test_velocity_ms_to_mph_83
+    assert_equal 114.08, Velocity::Velocity.new(51.0, 'ms').to_mph
+  end
+
+  def test_velocity_kh_to_kh_84
+    assert_equal 53.0, Velocity::Velocity.new(53.0, 'kh').to_kh
+  end
+
+  def test_velocity_kh_to_ms_85
+    assert_equal 11.67, Velocity::Velocity.new(42.0, 'kh').to_ms
+  end
+
+  def test_velocity_knot_to_mph_86
+    assert_equal 18.41, Velocity::Velocity.new(16.0, 'knot').to_mph
+  end
+
+  def test_velocity_knot_to_mph_87
+    assert_equal 63.29, Velocity::Velocity.new(55.0, 'knot').to_mph
+  end
+
+  def test_velocity_kh_to_ms_88
+    assert_equal 11.67, Velocity::Velocity.new(42.0, 'kh').to_ms
+  end
+
+  def test_velocity_ms_to_ms_89
+    assert_equal 37.0, Velocity::Velocity.new(37.0, 'ms').to_ms
+  end
+
+  def test_velocity_mph_to_mph_90
+    assert_equal 99.0, Velocity::Velocity.new(99.0, 'mph').to_mph
+  end
+
+  def test_velocity_kh_to_ms_91
+    assert_equal 27.5, Velocity::Velocity.new(99.0, 'kh').to_ms
+  end
+
+  def test_velocity_kh_to_mph_92
+    assert_equal 43.5, Velocity::Velocity.new(70.0, 'kh').to_mph
+  end
+
+  def test_velocity_knot_to_ms_93
+    assert_equal 49.39, Velocity::Velocity.new(96.0, 'knot').to_ms
+  end
+
+  def test_velocity_kh_to_kh_94
+    assert_equal 9.0, Velocity::Velocity.new(9.0, 'kh').to_kh
+  end
+
+  def test_velocity_knot_to_mph_95
+    assert_equal 39.13, Velocity::Velocity.new(34.0, 'knot').to_mph
+  end
+
+  def test_velocity_kh_to_kh_96
+    assert_equal 11.0, Velocity::Velocity.new(11.0, 'kh').to_kh
+  end
+
+  def test_velocity_kh_to_knot_97
+    assert_equal 19.44, Velocity::Velocity.new(36.0, 'kh').to_knot
+  end
+
+  def test_velocity_kh_to_ms_98
+    assert_equal 7.22, Velocity::Velocity.new(26.0, 'kh').to_ms
+  end
+
+  def test_velocity_kh_to_knot_99
+    assert_equal 19.44, Velocity::Velocity.new(36.0, 'kh').to_knot
+  end
+
+  def test_velocity_mph_to_ms_100
+    assert_equal 38.0, Velocity::Velocity.new(85.0, 'mph').to_ms
   end
 end
--- test/units/weight_test.rb
+++ test/units/weight_test.rb
@@ -7,111 +7,407 @@ class WeightTest < Minitest::Test
     assert_equal 1, 1
   end
 
-  def test_weight_po_to_ton_0
-    assert_equal 0.3715, Weight::Weight.new(819.0, 'po').to_ton
+  def test_weight_kg_to_po_0
+    assert_equal 17.6368, Weight::Weight.new(8.0, 'kg').to_po
   end
 
-  def test_weight_g_to_g_1
-    assert_equal 1_723.0, Weight::Weight.new(1_723.0, 'g').to_g
+  def test_weight_kg_to_ton_1
+    assert_equal 0.087, Weight::Weight.new(87.0, 'kg').to_ton
   end
 
-  def test_weight_ton_to_g_2
-    assert_equal 330_000_000.0, Weight::Weight.new(330.0, 'ton').to_g
+  def test_weight_oz_to_ton_2
+    assert_equal 0.00266, Weight::Weight.new(94.0, 'oz').to_ton
   end
 
-  def test_weight_ton_to_kg_3
-    assert_equal 910_000.0, Weight::Weight.new(910.0, 'ton').to_kg
+  def test_weight_ton_to_ton_3
+    assert_equal 53.0, Weight::Weight.new(53.0, 'ton').to_ton
   end
 
-  def test_weight_kg_to_g_4
-    assert_equal 1_489_000.0, Weight::Weight.new(1_489.0, 'kg').to_g
+  def test_weight_ton_to_g_4
+    assert_equal 14000000.0, Weight::Weight.new(14.0, 'ton').to_g
   end
 
-  def test_weight_ton_to_ton_5
-    assert_equal 852.0, Weight::Weight.new(852.0, 'ton').to_ton
+  def test_weight_oz_to_po_5
+    assert_equal 1.49998, Weight::Weight.new(24.0, 'oz').to_po
   end
 
-  def test_weight_ton_to_oz_6
-    assert_equal 69_348_684.0, Weight::Weight.new(1_966.0, 'ton').to_oz
+  def test_weight_ton_to_kg_6
+    assert_equal 60000.0, Weight::Weight.new(60.0, 'ton').to_kg
   end
 
-  def test_weight_ton_to_oz_7
-    assert_equal 15_273_642.0, Weight::Weight.new(433.0, 'ton').to_oz
+  def test_weight_ton_to_po_7
+    assert_equal 19841.4, Weight::Weight.new(9.0, 'ton').to_po
   end
 
-  def test_weight_oz_to_kg_8
-    assert_equal 6.43533, Weight::Weight.new(227.0, 'oz').to_kg
+  def test_weight_g_to_po_8
+    assert_equal 0.00882, Weight::Weight.new(4.0, 'g').to_po
   end
 
-  def test_weight_g_to_g_9
-    assert_equal 523.0, Weight::Weight.new(523.0, 'g').to_g
+  def test_weight_po_to_po_9
+    assert_equal 4.0, Weight::Weight.new(4.0, 'po').to_po
   end
 
-  def test_weight_oz_to_g_10
-    assert_equal 16_584.45314, Weight::Weight.new(585.0, 'oz').to_g
+  def test_weight_g_to_g_10
+    assert_equal 67.0, Weight::Weight.new(67.0, 'g').to_g
   end
 
-  def test_weight_ton_to_oz_11
-    assert_equal 54_780_522.0, Weight::Weight.new(1_553.0, 'ton').to_oz
+  def test_weight_po_to_kg_11
+    assert_equal 27.66942, Weight::Weight.new(61.0, 'po').to_kg
   end
 
-  def test_weight_po_to_oz_12
-    assert_equal 3_440.03901, Weight::Weight.new(215.0, 'po').to_oz
+  def test_weight_g_to_g_12
+    assert_equal 2.0, Weight::Weight.new(2.0, 'g').to_g
   end
 
-  def test_weight_po_to_po_13
-    assert_equal 1_582.0, Weight::Weight.new(1_582.0, 'po').to_po
+  def test_weight_po_to_ton_13
+    assert_equal 0.04264, Weight::Weight.new(94.0, 'po').to_ton
   end
 
-  def test_weight_g_to_kg_14
-    assert_equal 1.087, Weight::Weight.new(1_087.0, 'g').to_kg
+  def test_weight_po_to_kg_14
+    assert_equal 28.57661, Weight::Weight.new(63.0, 'po').to_kg
   end
 
-  def test_weight_ton_to_kg_15
-    assert_equal 445_000.0, Weight::Weight.new(445.0, 'ton').to_kg
+  def test_weight_kg_to_oz_15
+    assert_equal 1_058.22, Weight::Weight.new(30.0, 'kg').to_oz
   end
 
-  def test_weight_g_to_kg_16
-    assert_equal 0.721, Weight::Weight.new(721.0, 'g').to_kg
+  def test_weight_kg_to_kg_16
+    assert_equal 18.0, Weight::Weight.new(18.0, 'kg').to_kg
   end
 
-  def test_weight_po_to_ton_17
-    assert_equal 0.70126, Weight::Weight.new(1_546.0, 'po').to_ton
+  def test_weight_ton_to_g_17
+    assert_equal 34000000.0, Weight::Weight.new(34.0, 'ton').to_g
   end
 
-  def test_weight_kg_to_g_18
-    assert_equal 1_488_000.0, Weight::Weight.new(1_488.0, 'kg').to_g
+  def test_weight_ton_to_g_18
+    assert_equal 72000000.0, Weight::Weight.new(72.0, 'ton').to_g
   end
 
-  def test_weight_ton_to_g_19
-    assert_equal 1_451_000_000.0, Weight::Weight.new(1_451.0, 'ton').to_g
+  def test_weight_oz_to_ton_19
+    assert_equal 0.0015, Weight::Weight.new(53.0, 'oz').to_ton
   end
 
-  def test_weight_po_to_kg_20
-    assert_equal 16.78309, Weight::Weight.new(37.0, 'po').to_kg
+  def test_weight_po_to_po_20
+    assert_equal 91.0, Weight::Weight.new(91.0, 'po').to_po
   end
 
-  def test_weight_oz_to_ton_21
-    assert_equal 0.01622, Weight::Weight.new(572.0, 'oz').to_ton
+  def test_weight_kg_to_g_21
+    assert_equal 53000.0, Weight::Weight.new(53.0, 'kg').to_g
   end
 
-  def test_weight_kg_to_g_22
-    assert_equal 667_000.0, Weight::Weight.new(667.0, 'kg').to_g
+  def test_weight_ton_to_po_22
+    assert_equal 167549.6, Weight::Weight.new(76.0, 'ton').to_po
   end
 
   def test_weight_po_to_oz_23
-    assert_equal 2_000.02268, Weight::Weight.new(125.0, 'po').to_oz
+    assert_equal 1_120.0127, Weight::Weight.new(70.0, 'po').to_oz
   end
 
-  def test_weight_g_to_kg_24
-    assert_equal 0.8, Weight::Weight.new(800.0, 'g').to_kg
+  def test_weight_ton_to_oz_24
+    assert_equal 2_786_646.0, Weight::Weight.new(79.0, 'ton').to_oz
   end
 
-  def test_weight_po_to_g_25
-    assert_equal 652_726.11812, Weight::Weight.new(1_439.0, 'po').to_g
+  def test_weight_g_to_kg_25
+    assert_equal 0.003, Weight::Weight.new(3.0, 'g').to_kg
   end
 
-  def test_weight_g_to_ton_26
-    assert_equal 0.00075, Weight::Weight.new(745.0, 'g').to_ton
+  def test_weight_po_to_oz_26
+    assert_equal 832.00943, Weight::Weight.new(52.0, 'po').to_oz
+  end
+
+  def test_weight_ton_to_ton_27
+    assert_equal 5.0, Weight::Weight.new(5.0, 'ton').to_ton
+  end
+
+  def test_weight_ton_to_oz_28
+    assert_equal 2_963_016.0, Weight::Weight.new(84.0, 'ton').to_oz
+  end
+
+  def test_weight_ton_to_ton_29
+    assert_equal 33.0, Weight::Weight.new(33.0, 'ton').to_ton
+  end
+
+  def test_weight_kg_to_ton_30
+    assert_equal 0.022, Weight::Weight.new(22.0, 'kg').to_ton
+  end
+
+  def test_weight_ton_to_po_31
+    assert_equal 121_253.0, Weight::Weight.new(55.0, 'ton').to_po
+  end
+
+  def test_weight_oz_to_ton_32
+    assert_equal 0.0015, Weight::Weight.new(53.0, 'oz').to_ton
+  end
+
+  def test_weight_po_to_g_33
+    assert_equal 30_844.59766, Weight::Weight.new(68.0, 'po').to_g
+  end
+
+  def test_weight_kg_to_g_34
+    assert_equal 48_000.0, Weight::Weight.new(48.0, 'kg').to_g
+  end
+
+  def test_weight_ton_to_oz_35
+    assert_equal 1_904_796.0, Weight::Weight.new(54.0, 'ton').to_oz
+  end
+
+  def test_weight_po_to_po_36
+    assert_equal 58.0, Weight::Weight.new(58.0, 'po').to_po
+  end
+
+  def test_weight_g_to_ton_37
+    assert_equal 3.0e-05, Weight::Weight.new(30.0, 'g').to_ton
+  end
+
+  def test_weight_g_to_g_38
+    assert_equal 24.0, Weight::Weight.new(24.0, 'g').to_g
+  end
+
+  def test_weight_po_to_kg_39
+    assert_equal 20.41187, Weight::Weight.new(45.0, 'po').to_kg
+  end
+
+  def test_weight_ton_to_oz_40
+    assert_equal 423_288.0, Weight::Weight.new(12.0, 'ton').to_oz
+  end
+
+  def test_weight_oz_to_oz_41
+    assert_equal 67.0, Weight::Weight.new(67.0, 'oz').to_oz
+  end
+
+  def test_weight_kg_to_g_42
+    assert_equal 10_000.0, Weight::Weight.new(10.0, 'kg').to_g
+  end
+
+  def test_weight_oz_to_ton_43
+    assert_equal 0.00204, Weight::Weight.new(72.0, 'oz').to_ton
+  end
+
+  def test_weight_oz_to_oz_44
+    assert_equal 93.0, Weight::Weight.new(93.0, 'oz').to_oz
+  end
+
+  def test_weight_kg_to_g_45
+    assert_equal 57000.0, Weight::Weight.new(57.0, 'kg').to_g
+  end
+
+  def test_weight_oz_to_ton_46
+    assert_equal 0.00153, Weight::Weight.new(54.0, 'oz').to_ton
+  end
+
+  def test_weight_po_to_ton_47
+    assert_equal 0.00726, Weight::Weight.new(16.0, 'po').to_ton
+  end
+
+  def test_weight_po_to_oz_48
+    assert_equal 544.00617, Weight::Weight.new(34.0, 'po').to_oz
+  end
+
+  def test_weight_po_to_g_49
+    assert_equal 453.59702, Weight::Weight.new(1.0, 'po').to_g
+  end
+
+  def test_weight_kg_to_kg_50
+    assert_equal 20.0, Weight::Weight.new(20.0, 'kg').to_kg
+  end
+
+  def test_weight_ton_to_oz_51
+    assert_equal 3280482.0, Weight::Weight.new(93.0, 'ton').to_oz
+  end
+
+  def test_weight_ton_to_ton_52
+    assert_equal 82.0, Weight::Weight.new(82.0, 'ton').to_ton
+  end
+
+  def test_weight_g_to_ton_53
+    assert_equal 6.0e-05, Weight::Weight.new(63.0, 'g').to_ton
+  end
+
+  def test_weight_g_to_ton_54
+    assert_equal 9.0e-05, Weight::Weight.new(87.0, 'g').to_ton
+  end
+
+  def test_weight_kg_to_ton_55
+    assert_equal 0.093, Weight::Weight.new(93.0, 'kg').to_ton
+  end
+
+  def test_weight_po_to_oz_56
+    assert_equal 848.00962, Weight::Weight.new(53.0, 'po').to_oz
+  end
+
+  def test_weight_ton_to_g_57
+    assert_equal 37000000.0, Weight::Weight.new(37.0, 'ton').to_g
+  end
+
+  def test_weight_g_to_g_58
+    assert_equal 10.0, Weight::Weight.new(10.0, 'g').to_g
+  end
+
+  def test_weight_oz_to_ton_59
+    assert_equal 0.00187, Weight::Weight.new(66.0, 'oz').to_ton
+  end
+
+  def test_weight_oz_to_oz_60
+    assert_equal 11.0, Weight::Weight.new(11.0, 'oz').to_oz
+  end
+
+  def test_weight_g_to_po_61
+    assert_equal 0.0485, Weight::Weight.new(22.0, 'g').to_po
+  end
+
+  def test_weight_ton_to_ton_62
+    assert_equal 48.0, Weight::Weight.new(48.0, 'ton').to_ton
+  end
+
+  def test_weight_g_to_g_63
+    assert_equal 53.0, Weight::Weight.new(53.0, 'g').to_g
+  end
+
+  def test_weight_oz_to_kg_64
+    assert_equal 1.19068, Weight::Weight.new(42.0, 'oz').to_kg
+  end
+
+  def test_weight_po_to_oz_65
+    assert_equal 816.00925, Weight::Weight.new(51.0, 'po').to_oz
+  end
+
+  def test_weight_kg_to_kg_66
+    assert_equal 42.0, Weight::Weight.new(42.0, 'kg').to_kg
+  end
+
+  def test_weight_g_to_po_67
+    assert_equal 0.17637, Weight::Weight.new(80.0, 'g').to_po
+  end
+
+  def test_weight_po_to_g_68
+    assert_equal 907.19405, Weight::Weight.new(2.0, 'po').to_g
+  end
+
+  def test_weight_ton_to_kg_69
+    assert_equal 16000.0, Weight::Weight.new(16.0, 'ton').to_kg
+  end
+
+  def test_weight_kg_to_g_70
+    assert_equal 47000.0, Weight::Weight.new(47.0, 'kg').to_g
+  end
+
+  def test_weight_ton_to_po_71
+    assert_equal 108025.4, Weight::Weight.new(49.0, 'ton').to_po
+  end
+
+  def test_weight_g_to_oz_72
+    assert_equal 0.38801, Weight::Weight.new(11.0, 'g').to_oz
+  end
+
+  def test_weight_po_to_ton_73
+    assert_equal 0.04218, Weight::Weight.new(93.0, 'po').to_ton
+  end
+
+  def test_weight_oz_to_g_74
+    assert_equal 1474.17361, Weight::Weight.new(52.0, 'oz').to_g
+  end
+
+  def test_weight_oz_to_po_75
+    assert_equal 1.93748, Weight::Weight.new(31.0, 'oz').to_po
+  end
+
+  def test_weight_po_to_ton_76
+    assert_equal 0.03084, Weight::Weight.new(68.0, 'po').to_ton
+  end
+
+  def test_weight_po_to_ton_77
+    assert_equal 0.01633, Weight::Weight.new(36.0, 'po').to_ton
+  end
+
+  def test_weight_kg_to_ton_78
+    assert_equal 0.013, Weight::Weight.new(13.0, 'kg').to_ton
+  end
+
+  def test_weight_kg_to_g_79
+    assert_equal 32000.0, Weight::Weight.new(32.0, 'kg').to_g
+  end
+
+  def test_weight_oz_to_kg_80
+    assert_equal 1.50252, Weight::Weight.new(53.0, 'oz').to_kg
+  end
+
+  def test_weight_kg_to_g_81
+    assert_equal 59000.0, Weight::Weight.new(59.0, 'kg').to_g
+  end
+
+  def test_weight_po_to_oz_82
+    assert_equal 704.00798, Weight::Weight.new(44.0, 'po').to_oz
+  end
+
+  def test_weight_kg_to_g_83
+    assert_equal 66000.0, Weight::Weight.new(66.0, 'kg').to_g
+  end
+
+  def test_weight_kg_to_po_84
+    assert_equal 149.9128, Weight::Weight.new(68.0, 'kg').to_po
+  end
+
+  def test_weight_g_to_g_85
+    assert_equal 10.0, Weight::Weight.new(10.0, 'g').to_g
+  end
+
+  def test_weight_oz_to_oz_86
+    assert_equal 2.0, Weight::Weight.new(2.0, 'oz').to_oz
+  end
+
+  def test_weight_po_to_ton_87
+    assert_equal 0.01588, Weight::Weight.new(35.0, 'po').to_ton
+  end
+
+  def test_weight_oz_to_ton_88
+    assert_equal 0.00054, Weight::Weight.new(19.0, 'oz').to_ton
+  end
+
+  def test_weight_oz_to_oz_89
+    assert_equal 79.0, Weight::Weight.new(79.0, 'oz').to_oz
+  end
+
+  def test_weight_g_to_g_90
+    assert_equal 44.0, Weight::Weight.new(44.0, 'g').to_g
+  end
+
+  def test_weight_ton_to_po_91
+    assert_equal 194004.8, Weight::Weight.new(88.0, 'ton').to_po
+  end
+
+  def test_weight_kg_to_g_92
+    assert_equal 8000.0, Weight::Weight.new(8.0, 'kg').to_g
+  end
+
+  def test_weight_ton_to_kg_93
+    assert_equal 31000.0, Weight::Weight.new(31.0, 'ton').to_kg
+  end
+
+  def test_weight_oz_to_oz_94
+    assert_equal 87.0, Weight::Weight.new(87.0, 'oz').to_oz
+  end
+
+  def test_weight_po_to_oz_95
+    assert_equal 272.00308, Weight::Weight.new(17.0, 'po').to_oz
+  end
+
+  def test_weight_ton_to_po_96
+    assert_equal 6613.8, Weight::Weight.new(3.0, 'ton').to_po
+  end
+
+  def test_weight_kg_to_kg_97
+    assert_equal 11.0, Weight::Weight.new(11.0, 'kg').to_kg
+  end
+
+  def test_weight_ton_to_g_98
+    assert_equal 6000000.0, Weight::Weight.new(6.0, 'ton').to_g
+  end
+
+  def test_weight_ton_to_kg_99
+    assert_equal 76000.0, Weight::Weight.new(76.0, 'ton').to_kg
+  end
+
+  def test_weight_po_to_g_100
+    assert_equal 0.0, Weight::Weight.new(0.0, 'po').to_g
   end
 end
